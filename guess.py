f93ead37420af350b5f393a78671140bfb1de722
zwd <17326195691@163.com>
2024-03-26 15:26:45 +0800

    feat: update guess

--- guess.py
+++ guess.py
@@ -1,11 +1,20 @@
 """ 用pyhon设计第一个游戏 """
 
-temp = input("猜一猜我心里在想的哪个数字：")
-guess = int(temp)
+counts = 3
 
-if guess == 8:
-    print("牛啊牛啊，你猜对了！")
-else:
-    print("猜错了，我想的数字是8！")
+while counts > 0:
+    temp = input("猜一猜我心里在想的哪个数字：")
+    guess = int(temp)
+
+    if guess == 8:
+        print("牛啊牛啊，你猜对了！")
+        break
+    else:
+        print("猜错了！")
+        if guess < 8:
+            print("小啦")
+        else:
+            print("大啦")
+    counts = counts - 1
 
 print("游戏结束~")
